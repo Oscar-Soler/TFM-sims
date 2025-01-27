7d68ddfcbf78ab624d3235d167d90a742e38ba0c
Oscar Soler <alu0101327365@ull.edu.es>
2025-01-27 20:31:46 +0000

    Division in 1d and 2d, adding magnetic field for 1d only yet

---
+++ 1d/mhd_shock_test/mod_usr.f
@@ -0,0 +1,146 @@
+module mod_usr
+  use mod_mhd
+
+  implicit none
+
+contains
+
+  subroutine usr_init()
+
+      usr_init_one_grid => rm1d_init_one_grid
+
+      !usr_create_particles => generate_particles
+
+    call set_coordinate_system("Cartesian_1.75D")
+    call mhd_activate()
+
+  end subroutine usr_init
+
+  ! Initialize one grid
+  subroutine rm1d_init_one_grid(ixGmin1,ixGmax1,ixmin1,ixmax1,w,x)
+    integer, intent(in) :: ixGmin1,ixGmax1, ixmin1,ixmax1
+    double precision, intent(in) :: x(ixGmin1:ixGmax1,1:ndim)
+    double precision, intent(inout) :: w(ixGmin1:ixGmax1,1:nw)
+    ! hd variables for shocks  
+    double precision :: M, rho_eq, v_0, v_eq, p_eq, rho_1, v_1, v_sh, p_1, M1,&
+      cs_0, gamma
+    ! mhd variables
+    ! for a uniform magnetic field
+      double precision :: theta_b, phi_b, B0, b_0x, b_0y, b_0z, beta_0,&
+      B1, b_1x, b_1y, b_1z, vA0, vA1, X_value, mu_0=1,&
+      v_0x, v_0y, v_0z, v_1x, v_1y, v_1z
+    ! mhd and perpendicular / oblique shock switch
+    logical :: magnetic, perpendicular  
+    ! thread variables
+    double precision :: q_th, rho_th, x_th_ini, x_th_fin, x_sh
+
+      perpendicular = .true.
+      magnetic = .true.
+      M = 5.405405405405405d0
+      rho_eq = 1d0
+      p_eq = 1d0
+      
+      if (perpendicular .eqv. .true.) then
+         theta_b = 0
+         phi_b = 0
+      end if
+      
+      beta_0 = 1
+      X_value = 3.14954219493607
+
+      q_th = 100d0
+      x_th_ini = 2
+      x_th_fin = 3
+      x_sh = 0
+      
+      
+!------------------------------------- Above parameters, below calculations
+      !if (magnetic .eqv. .false.) gamma = hd_gamma
+      if (magnetic .eqv. .true.) gamma = mhd_gamma
+      
+    p_1 = p_eq * ((2*gamma)/(gamma+1)*M**2 - (gamma-1)/(gamma+1))
+    rho_1 = rho_eq / (2/((gamma+1)*M**2) + (gamma-1)/(gamma+1))
+    M1 = sqrt( (2+(gamma-1)*M**2) / (2*gamma*M**2 - (gamma-1)))
+    cs_0 = sqrt(gamma * p_eq / rho_eq)
+    !v_eq = sqrt(gamma * p_eq / rho_eq)
+    !v_0 = -M*cs_0
+      v_0 = 0
+      v_0x = 0
+    v_sh = v_0 + M*cs_0
+    v_1 = rho_eq/rho_1 * (v_0-v_sh) + v_sh
+    rho_th = rho_eq * q_th
+
+    ! mhd
+      if (magnetic .eqv. .true.) then
+         B0 = SQRT(2*mu_0*p_eq/beta_0)
+         vA0 =  B0/ SQRT(mu_0 * rho_eq)
+         if (perpendicular .eqv. .true.) then
+            b_0z = B0
+            b_0x = 0
+            b_0y = 0
+            rho_1 = rho_eq * X_value
+            v_1x = (v_0x-v_sh) / X_value + v_sh
+            v_1y = 0
+            !v2y = v1y * (v1**2-vA1**2)/(v1**2-X_sol*vA1**2)
+            v_1 = SQRT(v_1x**2 + v_1y**2)
+            b_1x = b_0x
+            b_1y = b_0y
+            b_1z = b_0z * X_value
+            p_1 = p_eq*(1+gamma*M**2*(1-1/X_value)+1/beta_0*(1-X_value**2))
+         end if
+      end if
+            
+      where (x(ixmin1:ixmax1,1) < x_sh)
+         w(ixmin1:ixmax1,rho_)   = rho_1
+         w(ixmin1:ixmax1,mom(1)) = v_1
+         w(ixmin1:ixmax1,p_)     = p_1
+      elsewhere ((x(ixmin1:ixmax1,1) > x_th_ini) .and. (x(ixmin1:ixmax1,1) < x_th_fin))
+         w(ixmin1:ixmax1,rho_)   = rho_th
+         w(ixmin1:ixmax1,mom(1)) = v_0
+         w(ixmin1:ixmax1,p_)     = p_eq
+      elsewhere
+         w(ixmin1:ixmax1,rho_)   = rho_eq
+         w(ixmin1:ixmax1,mom(1)) = v_0
+         w(ixmin1:ixmax1,p_)     = p_eq
+      end where
+      
+      if (magnetic .eqv. .true.) then
+         where (x(ixmin1:ixmax1,1) < x_sh)
+            w(ixmin1:ixmax1,mag(1))   = b_1x
+            w(ixmin1:ixmax1,mag(2))   = b_1y
+            w(ixmin1:ixmax1,mag(3))   = b_1z
+         elsewhere ((x(ixmin1:ixmax1,1) > x_th_ini) .and. (x(ixmin1:ixmax1,1) < x_th_fin))
+            w(ixmin1:ixmax1,mag(1))   = b_0x
+            w(ixmin1:ixmax1,mag(2))   = b_0y
+            w(ixmin1:ixmax1,mag(3))   = b_0z
+         elsewhere
+            w(ixmin1:ixmax1,mag(1))   = b_0x
+            w(ixmin1:ixmax1,mag(2))   = b_0y
+            w(ixmin1:ixmax1,mag(3))   = b_0z
+         end where
+      end if
+
+    if (magnetic .eqv. .false.) call hd_to_conserved(ixGmin1,ixGmax1,ixmin1,ixmax1,w,x)
+    if (magnetic .eqv. .true.) call mhd_to_conserved(ixGmin1,ixGmax1,ixmin1,ixmax1,w,x)
+
+  end subroutine rm1d_init_one_grid
+
+subroutine generate_particles(n_particles, x, v, q, m, follow)
+  integer, intent(in)           :: n_particles
+  double precision, intent(out) :: x(3, n_particles)
+  double precision, intent(out) :: v(3, n_particles)
+  double precision, intent(out) :: q(n_particles)
+  double precision, intent(out) :: m(n_particles)
+  logical, intent(out)          :: follow(n_particles)
+  integer :: i
+
+      do i = 1, n_particles
+         follow(i) = .true.
+      end do
+
+      do i= 1, n_particles
+         x(1,i) = -2 + i
+      end do
+end subroutine generate_particles
+
+end module mod_usr
---
+++ 1d/thread_thin_vs_2d/mod_usr.f
@@ -0,0 +1,89 @@
+module mod_usr
+  use mod_hd
+
+  implicit none
+
+contains
+
+  subroutine usr_init()
+
+      usr_init_one_grid => rm1d_init_one_grid
+
+      usr_create_particles => generate_particles
+
+    call set_coordinate_system("Cartesian")
+    call hd_activate()
+
+  end subroutine usr_init
+
+  ! Initialize one grid
+  subroutine rm1d_init_one_grid(ixGmin1,ixGmax1,ixmin1,ixmax1,w,x)
+    integer, intent(in) :: ixGmin1,ixGmax1, ixmin1,ixmax1
+    double precision, intent(in) :: x(ixGmin1:ixGmax1,1:ndim)
+    double precision, intent(inout) :: w(ixGmin1:ixGmax1,1:nw)
+    double precision :: M, rho_eq, v_0, v_eq, p_eq, rho_1, v_1, v_sh, p_1, M1,&
+      cs_0
+    double precision :: q_th, rho_th, x_th_ini, x_th_fin, x_sh
+
+    M = 5.405405405405405d0
+    rho_eq = 1d0
+    p_eq = 1d0
+
+    q_th = 100d0
+      x_th_ini = 2
+      x_th_fin = 3
+      x_sh = 0
+      
+    !------------------------------------- Above parameters, below calculations
+      
+    p_1 = p_eq * ((2*hd_gamma)/(hd_gamma+1)*M**2 - (hd_gamma-1)/(hd_gamma+1))
+    rho_1 = rho_eq / (2/((hd_gamma+1)*M**2) + (hd_gamma-1)/(hd_gamma+1))
+    M1 = sqrt( (2+(hd_gamma-1)*M**2) / (2*hd_gamma*M**2 - (hd_gamma-1)))
+    cs_0 = sqrt(hd_gamma * p_eq / rho_eq)
+    !v_eq = sqrt(hd_gamma * p_eq / rho_eq)
+    !v_0 = -M*cs_0
+    v_0 = 0
+    v_sh = v_0 + M*cs_0
+    v_1 = rho_eq/rho_1 * (v_0-v_sh) + v_sh
+      !print *, rho_1/rho_eq
+      !print *, p_1/p_eq
+    rho_th = rho_eq * q_th
+      
+    where (x(ixmin1:ixmax1,1) < x_sh)
+        w(ixmin1:ixmax1,rho_)   = rho_1
+        w(ixmin1:ixmax1,mom(1)) = v_1
+        w(ixmin1:ixmax1,p_)     = p_1
+    elsewhere ((x(ixmin1:ixmax1,1) > x_th_ini) .and. (x(ixmin1:ixmax1,1) < x_th_fin))
+        w(ixmin1:ixmax1,rho_)   = rho_th
+        w(ixmin1:ixmax1,mom(1)) = v_0
+        w(ixmin1:ixmax1,p_)     = p_eq
+    elsewhere
+        w(ixmin1:ixmax1,rho_)   = rho_eq
+        w(ixmin1:ixmax1,mom(1)) = v_0
+        w(ixmin1:ixmax1,p_)     = p_eq
+     
+    end where
+    
+    call hd_to_conserved(ixGmin1,ixGmax1,ixmin1,ixmax1,w,x)
+
+  end subroutine rm1d_init_one_grid
+
+subroutine generate_particles(n_particles, x, v, q, m, follow)
+  integer, intent(in)           :: n_particles
+  double precision, intent(out) :: x(3, n_particles)
+  double precision, intent(out) :: v(3, n_particles)
+  double precision, intent(out) :: q(n_particles)
+  double precision, intent(out) :: m(n_particles)
+  logical, intent(out)          :: follow(n_particles)
+  integer :: i
+
+      do i = 1, n_particles
+         follow(i) = .true.
+      end do
+
+      do i= 1, n_particles
+         x(1,i) = -2 + i
+      end do
+end subroutine generate_particles
+
+end module mod_usr
