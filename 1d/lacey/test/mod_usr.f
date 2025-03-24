adf943990db3192a2dfcc505677ff22ae027e42a
Oscar Soler <alu0101327365@ull.edu.es>
2025-03-24 20:45:24 +0000

    Large update, up to thermal conduction tests of Lacey theory

---
+++ 1d/lacey/test/mod_usr.f
@@ -0,0 +1,206 @@
+module mod_usr
+  use mod_hd
+
+      implicit none
+
+      !double precision :: heatunit, bQ0
+
+contains
+
+  subroutine usr_init()
+
+      usr_init_one_grid => rm1d_init_one_grid
+
+      call set_coordinate_system("Cartesian")
+
+
+      unit_length         = 1d9!/8d-7
+      unit_numberdensity  = 1d9
+      unit_temperature    = 1d6
+
+      !usr_source          => special_source
+
+      
+      call hd_activate()
+
+  end subroutine usr_init
+
+  ! Initialize one grid
+  subroutine rm1d_init_one_grid(ixGmin1,ixGmax1,ixmin1,ixmax1,w,x)
+    integer, intent(in) :: ixGmin1,ixGmax1, ixmin1,ixmax1
+    double precision, intent(in) :: x(ixGmin1:ixGmax1,1:ndim)
+    double precision, intent(inout) :: w(ixGmin1:ixGmax1,1:nw)
+      double precision :: rho0, p0, v0, tau0, T0
+      integer, parameter :: n = 2048
+      integer :: i, ios
+      double precision, allocatable :: xi(:), eta(:), tau(:), p(:), rho(:)
+
+!      heatunit=unit_pressure/unit_time !3.697693390805347E-003 erg*cm-3/s,erg*cm-3/s
+!      bQ0=6.0d-5/heatunit       !background heating power density !MLB lo he dividido entre 2, 1d-4
+
+
+      allocate(xi(n), eta(n), tau(n), p(n), rho(n))
+      
+! pre-shock status determined only to find tau0, values used for initial state
+! are given in terms of eta, adimensional
+      T0 = 1.1e6
+      rho0 = 4e-16
+      p0 = 6.2e-2
+      v0 = 2.5e7
+      tau0 = p0/rho0/v0**2
+      
+      !tau0 = 0.25
+! reading of initial conditions in .par file
+
+!     open(10, file="initial_conditions.dat", status="old",action="read")
+!      do i = 1, n
+!         read(10, *, IOSTAT=ios) xi(i), eta(i)
+!         !print*, xi(i), eta(i)
+!        if (ios /= 0) exit  ! Stop reading if end of file is reached
+!     end do
+
+      eta = 1
+
+      rho = 1/eta
+      tau = eta*(1+tau0-eta)
+      p = tau/tau0 * rho
+
+      print*, xprobmin1, xprobmax1
+      print*, domain_nx1
+      print*, size(x)
+      print*, size(eta)
+      print*, ixmin1, ixmax1
+      print*, size(w(ixmin1:ixmax1,p_))
+      
+      w(ixmin1:ixmax1,rho_)   = rho
+      w(ixmin1:ixmax1,mom(1)) = eta * rho
+      w(ixmin1:ixmax1,p_)     = p
+
+      
+      !print*, w(ixmin1:ixmax1,p_)
+      !print*, w(ixmin1:ixmax1,rho_)
+      !print*, w(ixmin1:ixmax1,mom(1))
+
+    
+    call hd_to_conserved(ixGmin1,ixGmax1,ixmin1,ixmax1,w,x)
+  end subroutine rm1d_init_one_grid
+!
+!      
+! subroutine special_source(qdt,ixImin1,ixImax1,ixOmin1,ixOmax1,iwmin,iwmax,qtC,wCT,qt,w,x)
+!      integer, intent(in) :: ixImin1,ixImax1, ixOmin1,ixOmax1, iwmin,iwmax
+!      double precision, intent(in) :: qdt, qtC, qt
+!      double precision, intent(in) :: x(ixImin1:ixImax1,1:ndim),wCT(ixImin1:ixImax1,1:nw)
+!      double precision, intent(inout) :: w(ixImin1:ixImax1,1:nw)
+!      double precision :: lQgrid(ixImin1:ixImax1)
+!
+!! add local heating lQ
+!      
+!      call getlQ(lQgrid,ixImin1,ixImax1,ixOmin1,ixOmax1,qt,wCT,x)
+!      w(ixOmin1:ixOmax1,e_)=w(ixOmin1:ixOmax1,e_)+qdt*lQgrid(ixOmin1:ixOmax1)
+!      end subroutine special_source
+!
+!
+!      
+!
+!   subroutine getlQ(lQgrid,ixImin1,ixImax1,ixOmin1,ixOmax1,qt,w,x)
+!  ! calculate localized heating lQ
+!  !nueva subrutina MLB
+!    integer, intent(in) :: ixImin1,ixImax1, ixOmin1,ixOmax1
+!    double precision, intent(in) :: qt, x(ixImin1:ixImax1,&
+!       1:ndim), w(ixImin1:ixImax1,1:nw)
+!
+!    double precision :: lQgrid(ixImin1:ixImax1),&
+!        A(ixImin1:ixImax1), lQd, lQ0, hp, lQt,Atop,Alow
+!    double precision :: auxradius1(ixImin1:ixImax1),xcenter1,auxm1
+!       !ycenter1,y11(ixImin1:ixImax1),y12(ixImin1:ixImax1)
+!    double precision :: radiuscentral(ixImin1:ixImax1)
+!    double precision :: auxradius2(ixImin1:ixImax1),xcenter2,auxm2
+!       !ycenter2,,y21(ixImin1:ixImax1,ixImin2:ixImax2),y22(ixImin1:ixImax1,&
+!       !ixImin2:ixImax2)
+!    double precision :: lambdamin,lambdamax,lambdamax2,lambda1(ixImin1:ixImax1),lambda11(ixImin1:ixImax1)
+!    double precision :: lambda2(ixImin1:ixImax1),&
+!       lambda22(ixImin1:ixImax1),xprimaL(ixImin1:ixImax1),zprimaL(ixImin1:ixImax1)
+!    double precision :: xprimaR(ixImin1:ixImax1), zprimaR(ixImin1:ixImax1)
+!    double precision :: thetaangle01,thetaangle1(ixImin1:ixImax1),thetaangle02,thetaangle2(ixImin1:ixImax1)
+!    double precision :: lQgridleft(ixImin1:ixImax1),lQgridright(ixImin1:ixImax1)
+!    double precision :: recta1(ixImin1:ixImax1),recta2(ixImin1:ixImax1)
+!    double precision :: sigma_heating,sigma_heating_impulsive,y1,lam1
+!    double precision :: impulsiveheatingfactor,impulsiveheatingduration,&
+!       timeimpulse
+!       double precision :: impulsiveheatingfactoramplitude, A0
+!      
+!       double precision :: heating_half, heating_start, heating_end
+!
+!    lQ0= 5.d-1/heatunit ! 8.d-3/heatunit !OSP!5.d0*1.d-2/heatunit!MLB 1.d-2/heatunit
+!    lQt=10.d0!5.d2/unit_time !MLB 5.d2/unit_time
+!    lQd=0.2d0 !MLB 0.2d0
+!    hp=0.22d0
+!    lQgrid=0.d0
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!  ! lambda
+!    lambdamin=0.1d0
+!    lambdamax=0.320d0
+!    lambdamax2=500.d0 !es esencialmente infinita
+!
+!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!! Calentamiento tipo Zhou et al. 2023
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!      sigma_heating=0.3d0
+!      sigma_heating_impulsive=0.15d0
+!      y1=0.22d0
+!      lam1=1.0d0!0.2d0!0.32d0
+!      xcenter1=0.0!-5.2288454d0 !LEFT
+!      !xcenter2= 8.5744060!5.2288454d0  !RIGHT
+!      A0=-132.0d0 !vector potential at the center of the field lines bundle
+!      !
+!      ! !impulsive heating
+!      impulsiveheatingfactor=1.d0
+!      impulsiveheatingduration=1.d0 ! every unit is 85 secs
+!      timeimpulse=3.001d0
+!      impulsiveheatingfactoramplitude=0.d0
+!      if(qt>timeimpulse .and. qt<timeimpulse + impulsiveheatingduration) then
+!        if(qt<timeimpulse+impulsiveheatingduration/4.d0) then
+!          impulsiveheatingfactor=1.d0+impulsiveheatingfactoramplitude* &
+!             (4.d0*(qt-timeimpulse)/impulsiveheatingduration)
+!        endif
+!        if(qt>=timeimpulse+impulsiveheatingduration/4.d0 .and. qt < &
+!           timeimpulse+impulsiveheatingduration*3.d0/4.d0) then
+!         impulsiveheatingfactor=1.d0+impulsiveheatingfactoramplitude
+!        endif
+!        if(qt>=timeimpulse+impulsiveheatingduration*3.d0/4.d0 .and. &
+!           qt<timeimpulse+impulsiveheatingduration) then
+!         impulsiveheatingfactor=1.d0+impulsiveheatingfactoramplitude* &
+!            (-4.d0/impulsiveheatingduration*(qt-timeimpulse-&
+!            3.d0/4.d0*impulsiveheatingduration)+1.d0)
+!        endif
+!      
+!      endif
+!
+!      !!CUIDADO HAY UNA ASIMETRÍA EN EL HEATING DEL 0.75 POR UNO.
+!      lQgrid(ixOmin1:ixOmax1)=lQ0*(impulsiveheatingfactor*dexp(&
+!      -(x(ixOmin1:ixOmax1,1)-xcenter1)**2/sigma_heating**2))
+!
+!! !MLB
+!!OSP dimensional time in s for beginning and end of heating
+!      heating_start = 100d0 / unit_time
+!      heating_end = 200d0 / unit_time
+!      heating_half = (heating_end + heating_start)/2
+!      
+!      if(qt<heating_start) then
+!         lQgrid(ixOmin1:ixOmax1)=0.d0
+!      endif
+!      if(qt>heating_start .and. qt< heating_half) then
+!         lQgrid(ixOmin1:ixOmax1)=(qt-heating_start)/(heating_half-heating_start)*lQgrid(ixOmin1:ixOmax1)
+!      endif
+!
+!      if(qt>heating_half .and. qt<heating_end) then
+!         lQgrid(ixOmin1:ixOmax1)=(heating_end-qt)/(heating_end-heating_half)*lQgrid(ixOmin1:ixOmax1)
+!      endif
+!      if(qt>heating_end) then
+!         lQgrid(ixOmin1:ixOmax1)=0.d0
+!      endif      
+!  end subroutine getlQ
+
+      
+end module mod_usr
--- 1d/mhd_shock_test/mod_usr.f
+++ 1d/mhd_shock_test/mod_usr.f
@@ -24,29 +24,35 @@ contains
     ! hd variables for shocks  
     double precision :: M, rho_eq, v_0, v_eq, p_eq, rho_1, v_1, v_sh, p_1, M1,&
       cs_0, gamma
+    double precision :: pi = ACOS(-1d0)
     ! mhd variables
     ! for a uniform magnetic field
       double precision :: theta_b, phi_b, B0, b_0x, b_0y, b_0z, beta_0,&
       B1, b_1x, b_1y, b_1z, vA0, vA1, X_value, mu_0=1,&
-      v_0x, v_0y, v_0z, v_1x, v_1y, v_1z
+      v_0x, v_0y, v_0z, v_1x, v_1y, v_1z, V_y_ref = 0, u_0, u_1
     ! mhd and perpendicular / oblique shock switch
-    logical :: magnetic, perpendicular  
+    logical :: magnetic, perpendicular, parallel, oblique  
     ! thread variables
     double precision :: q_th, rho_th, x_th_ini, x_th_fin, x_sh
 
-      perpendicular = .true.
+      perpendicular = .false.
+      parallel = .true.
+      oblique = .false.
       magnetic = .true.
       M = 5.405405405405405d0
       rho_eq = 1d0
       p_eq = 1d0
       
       if (perpendicular .eqv. .true.) then
-         theta_b = 0
-         phi_b = 0
+         !theta_b = pi/2
+         phi_b = pi/2
       end if
       
       beta_0 = 1
       X_value = 3.14954219493607
+
+      v_0x = 0
+      v_0y = 0
 
       q_th = 100d0
       x_th_ini = 2
@@ -57,50 +63,80 @@ contains
 !------------------------------------- Above parameters, below calculations
       !if (magnetic .eqv. .false.) gamma = hd_gamma
       if (magnetic .eqv. .true.) gamma = mhd_gamma
-      
-    p_1 = p_eq * ((2*gamma)/(gamma+1)*M**2 - (gamma-1)/(gamma+1))
-    rho_1 = rho_eq / (2/((gamma+1)*M**2) + (gamma-1)/(gamma+1))
-    M1 = sqrt( (2+(gamma-1)*M**2) / (2*gamma*M**2 - (gamma-1)))
-    cs_0 = sqrt(gamma * p_eq / rho_eq)
-    !v_eq = sqrt(gamma * p_eq / rho_eq)
-    !v_0 = -M*cs_0
-      v_0 = 0
-      v_0x = 0
-    v_sh = v_0 + M*cs_0
-    v_1 = rho_eq/rho_1 * (v_0-v_sh) + v_sh
-    rho_th = rho_eq * q_th
+
+      cs_0 = sqrt(gamma * p_eq / rho_eq)
+      v_sh = v_0 + M*cs_0
+      v_1y = v_0y
+
+      if (parallel .eqv. .true.) then
+         p_1 = p_eq * ((2*gamma)/(gamma+1)*M**2 - (gamma-1)/(gamma+1))
+         rho_1 = rho_eq / (2/((gamma+1)*M**2) + (gamma-1)/(gamma+1))
+!     M1 = sqrt( (2+(gamma-1)*M**2) / (2*gamma*M**2 - (gamma-1)))
+!     v_eq = sqrt(gamma * p_eq / rho_eq)
+!     v_0 = -M*cs_0
+!         v_0 = 0
+         v_1x = rho_eq/rho_1 * (v_0-v_sh) + v_sh
+      end if
+
+      rho_th = rho_eq * q_th
 
     ! mhd
       if (magnetic .eqv. .true.) then
          B0 = SQRT(2*mu_0*p_eq/beta_0)
          vA0 =  B0/ SQRT(mu_0 * rho_eq)
-         if (perpendicular .eqv. .true.) then
-            b_0z = B0
-            b_0x = 0
+
+         if ((perpendicular .eqv. .true.) .or. (oblique .eqv. .true.)) then
+            b_0z = B0* COS(theta_b)
+            b_0x = B0* SIN(theta_b)*COS(phi_b)
+            b_0y = B0* SIN(theta_b)*SIN(phi_b)
+         end if
+         
+
+         if (parallel .eqv. .true.) then
+            b_0x = B0
             b_0y = 0
+            b_0z = 0
+            b_1x = B0
+            b_1y = 0
+            b_1z = 0
+         end if
+         if (oblique .eqv. .true.)  V_y_ref = v_0y - (v_0x - v_sh) * b_0x/b_0y
+         u_0 = SQRT( (v_0x-v_sh)**2 + (v_0y - V_y_ref)**2)
+         v_1y = (v_0y-V_y_ref) * (u_0**2-vA0**2)/(u_0**2-X_value*vA0**2) + V_y_ref
+
+         if ((perpendicular .eqv. .true.) .or. (oblique .eqv. .true.))then
             rho_1 = rho_eq * X_value
             v_1x = (v_0x-v_sh) / X_value + v_sh
-            v_1y = 0
-            !v2y = v1y * (v1**2-vA1**2)/(v1**2-X_sol*vA1**2)
-            v_1 = SQRT(v_1x**2 + v_1y**2)
+            
             b_1x = b_0x
-            b_1y = b_0y
-            b_1z = b_0z * X_value
-            p_1 = p_eq*(1+gamma*M**2*(1-1/X_value)+1/beta_0*(1-X_value**2))
+            if (perpendicular .eqv. .true.) then
+               b_1y = b_0y * X_value
+               b_1z = b_0z * X_value
+               p_1 = p_eq*(1+gamma*M**2*(1-1/X_value)+1/beta_0*(1-X_value**2))
+            end if
+            if (oblique .eqv. .true.) then
+               u_1 =  SQRT( (v_1x-v_sh)**2 + (v_1y - V_y_ref)**2)
+               b_1y = b_0y * (u_0**2-vA0**2)*X_value/(u_0**2-X_value*vA0**2)
+               p_1 = p_eq * (X_value + (gamma-1)*X_value*u_0**2/(2*cs_0**2)*(1-u_1**2/u_0**2))
+            end if
+            
          end if
       end if
             
       where (x(ixmin1:ixmax1,1) < x_sh)
          w(ixmin1:ixmax1,rho_)   = rho_1
-         w(ixmin1:ixmax1,mom(1)) = v_1
+         w(ixmin1:ixmax1,mom(1)) = v_1x
+         w(ixmin1:ixmax1,mom(2)) = v_1y
          w(ixmin1:ixmax1,p_)     = p_1
       elsewhere ((x(ixmin1:ixmax1,1) > x_th_ini) .and. (x(ixmin1:ixmax1,1) < x_th_fin))
          w(ixmin1:ixmax1,rho_)   = rho_th
-         w(ixmin1:ixmax1,mom(1)) = v_0
+         w(ixmin1:ixmax1,mom(1)) = v_0x
+         w(ixmin1:ixmax1,mom(2)) = v_0y
          w(ixmin1:ixmax1,p_)     = p_eq
       elsewhere
          w(ixmin1:ixmax1,rho_)   = rho_eq
-         w(ixmin1:ixmax1,mom(1)) = v_0
+         w(ixmin1:ixmax1,mom(1)) = v_0x
+         w(ixmin1:ixmax1,mom(2)) = v_0y
          w(ixmin1:ixmax1,p_)     = p_eq
       end where
       
